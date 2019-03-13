f37c9d7fcc874b5767d66c1d13659cd185b61566
gabriel <gabriel@gabriel-notebook>
2019-03-13 20:45:43 -0300

    Exercicio 1 correção de váriaveis

---
+++ git/src/gabrielsoares/Exercicio1_Aula4.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gabrielsoares;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Exercicio1_Aula4 {
+    public static void main(String[] args){
+        // int x = 34.5; Variáveis inteiras não recebem números flutantes
+        // boolean resp = x; Variável boolean só recebe V ou F
+        int g = 17; // Está correta
+        int y = g; // Irá imprimir o código da letra
+        System.out.println(+g);
+        y = y + 10;
+        System.out.println(+y);
+        double t = 45;
+        // float p = 32.7; Falta a indicação de que tipo essa váriavel é
+        char resp;
+        // String n = 'ana'; Variáveis String devem ter seu valor atribuido com aspas duplas 
+    }
+}
