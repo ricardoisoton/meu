e4a559e047b1354e4a32f0518002366a61e608c8
19121005 <19121005@FDB-LABIN511n.facdombosco.com.br>
2019-03-13 20:06:41 -0300

    v1

---
+++ git/src/Ruan/OperadoresAritmeticos.java
@@ -0,0 +1,28 @@
+package Ruan;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 19121005
+ */
+public class OperadoresAritmeticos {
+   public static void main(String[]args){
+        int a=40,b=20,soma,mult,div,resto,sub;
+            soma = a + b;
+            mult = a *b;
+            div = a/b;
+            resto = a % b;
+            sub = a-b;
+            
+            System.out.println(soma);
+            System.out.println(mult);
+            System.out.println(div);
+            System.out.println(resto);
+            System.out.println(sub);
+     }
+  }
