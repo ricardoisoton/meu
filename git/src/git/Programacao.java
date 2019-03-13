1a2668578c5f1c920bb2d465221b785039708b16
lianderson <lianderson@FDB-LABIN501n.facdombosco.com.br>
2019-03-13 19:15:16 -0300

    teste

---
+++ git/src/git/Programacao.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git;
+
+/**
+ *
+ * @author lianderson
+ */
+public class Programacao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int idade =0;
+        String nome = "Lianderson";
+        System.out.println("Hello World!!");
+        System.out.print("Meu nome é Lianderson");
+        
+        
+        idade = 15;
+        idade = 12;
+        System.out.print("minha idade é="+idade+" anos");
+        
+        
+        
+        
+    }
+    
+}
