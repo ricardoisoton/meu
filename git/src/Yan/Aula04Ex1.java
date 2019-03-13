1f1058c823952b0e306a6517224d0e25041493d1
19113075 <19113075@FDB-LABIN513n.facdombosco.com.br>
2019-03-13 20:46:21 -0300

    ex1aula4

---
+++ git/src/Yan/Aula04Ex1.java
@@ -0,0 +1,21 @@
+package Yan;
+
+public class Aula04Ex1 {
+    
+    public static void main(String[] args){
+        
+        int x = 34.5;     //Não vai .
+        boolean resp = x; //Só aceita true e false
+        int g = 17;
+        int y = g;
+        y = y + 10;       
+        double t =45;
+        float p = 32.7;   //Faltou f no final
+        char resp;        //Faltou caractere unica
+        String n = 'ana'; //Faltou ""
+        
+        
+    }
+    
+    
+}
