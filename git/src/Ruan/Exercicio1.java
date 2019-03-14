86c485bcc06a3c567337960cfe79228ca29ada5d
19121005 <19121005@FDB-LABIN511n.facdombosco.com.br>
2019-03-13 22:05:09 -0300

    v1

--- git/src/Ruan/Exercicio1.java
+++ git/src/Ruan/Exercicio1.java
@@ -11,13 +11,16 @@ package Ruan;
  * @author 19121005
  */
 public class Exercicio1 {
+    public static void main(String[] args) {
     int x=34.5;          //Inteiros nao aceita numeros com virgulas
     boolean resp = x;   // Boolean aceita somente true ou false
     int g =17; 
     int y = g;
-    y = y +10; // Nao declarado um tipo
+    y = y + 10; 
     double t = 45; 
-    float p = 32.7f;   // falta F no final do valor
+    float p = 32.7;   // falta F no final do valor
     char resp; // tipo char aceita apenas uma letra ( M ou F)
     String n = 'ana'; // Esta faltando duas aspas "" para String
+    
+    }
 }
