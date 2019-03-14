55b615167a00d2acc2988884851c95bdb6ead69d
19113081 <19113081@FDB-LABIN514n.facdombosco.com.br>
2019-03-13 21:36:03 -0300

    versão 1.1

---
+++ git/src/ricardoisoton/aula1303exercicio2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ricardoisoton;
+
+/**
+ *
+ * @author 19113081
+ */
+public class aula1303exercicio2 {
+public static void main (String[] args) {
+    
+    int a123 = 1;
+    int sal* = 3;
+    int 1interface = 2;
+    int _nome = 4;
+    int $$a = 7;
+    int valores reais = 8;
+    
+    
+}
+}
