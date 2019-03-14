5aba02ff6ba237fd3da541f37c580a5052b61c91
pedro <pedro@LAPTOP-2J0ED1P2>
2019-03-13 21:37:04 -0300

    versao 1.o

---
+++ git/src/Pedro/Exercicio2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pedro;
+
+/**
+ *
+ * @author pedro
+ */
+public class Exercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a123=0;
+        // tudo certo
+        int sal*=0 ;
+        // errado ter um vezes
+        int 1interface=0;
+        // errado começar por numero
+        int _nome=0;
+        int $$a=0;
+        int valores reais;
+        //errado palavras separadas
+        
+        
+                
+        
+    }
+    
+}
